246bbb96781f6571a5a24c11449f5e47d61f8c5a
nice <s09222@kcis.com.tw>
2022-07-28 21:17:18 +0800

    7/28

---
+++ nice-game.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner a = new Scanner(System.in);
+        Scanner b = new Scanner(System.in);
+        Scanner c = new Scanner(System.in);
+        String a = a.nextLine();
+        int b = b.nextInt();
+        String c = c.nextLine();
+        for(i=0 ;i<a.length; i++ )
+        {
+            if(a.charAt(i).equals" ")
+            {
+                x=a.substring(0,i)
+                y=a.substring(i,)
+            }
+        }
+        int x =
+        int y = 
+        int step = 0
+        while(step<b)
+        {
+
+        }
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
