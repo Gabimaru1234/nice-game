ac2d0eb1152293958fc000e46b1040c98dd0f3da
nice <s09222@kcis.com.tw>
2022-08-13 20:11:55 +0800

    8/13

--- nice-game.java
+++ nice-game.java
@@ -9,38 +9,43 @@ import java.math.*;
 class Solution {
 
     public static void main(String args[]) {
+        
         Scanner in = new Scanner(System.in);
+        //數字近來
         int w = in.nextInt();
+        
         int z = int nextInt();
-        String array[][] nice = new String[][];
-        String q = String nextLine();
-        for(int p= 0 ; p< q.length(); p++)
-        {
-            for(int g =0; g< g[p].length(); g++)
-            {
-                nice[p][g]= q;
-            }
-        }
+        //make q into array
+        
+        int q = String nextLine();
 
 
-
+        
         for(int i=0; i<w.length();i++)
         {
             if(i.substring(i).equals(" "))
             {
                 int x = i.substring(0,i)
                 int y = i.substring(i,w.length())
+                //x 跟 y 
             }
         }
         int cnt =0;
+        int nice =0;
+        //如果變方向，
+        //0就是往上，
+        //1就是往右，
+        //2上往下，
+        //3是往左
         }
-        for(int a = 0; a < z ; a++)
+        while(cnt < z)
         {
-            if(y-1.equals("#"))
-            {
-
-            }
+            if(y  -1)
         }
+        // 這裡寫如果 上面有# 或是碰到邊界 
+        // 方向往右轉
+        //如果沒有 就cnt +1
+        // 
 
         // Write an answer using System.out.println()
         // To debug: System.err.println("Debug messages...");
